f65ffcca95ee0420a400297ca5cf0818defc04ea
Quentin Creuzet <quentin@f451.faith>
2019-06-26 13:43:01 +0200

    Base JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
+import 'nodelist-foreach-polyfill';
+import 'mdn-polyfills/Array.prototype.forEach';
+import 'mdn-polyfills/Array.prototype.find';
+import 'mdn-polyfills/Array.prototype.includes';
+import 'mdn-polyfills/String.prototype.includes';
+import lazySizes from 'lazysizes';
+
+import Website from './_modules/website';
 import Home from './_modules/home';
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener('DOMContentLoaded', function() {
+  Website.init();
   Home.init();
 });
