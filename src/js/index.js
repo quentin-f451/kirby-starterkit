a64c222894ef2cba35e044bf468e90477f054ec8
Quentin Creuzet <quentin@f451.faith>
2020-04-09 18:09:58 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,14 @@ import 'mdn-polyfills/Array.prototype.find';
 import 'mdn-polyfills/Array.prototype.includes';
 import 'mdn-polyfills/String.prototype.includes';
 import lazySizes from 'lazysizes';
+import jquery from "jquery";
+
+window.$ = window.jQuery = jquery;
 
 import Website from './_modules/website';
 import Home from './_modules/home';
 
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
   Website.init();
   Home.init();
 });
