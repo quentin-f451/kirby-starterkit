1f8ec81a4518ecfef365836b13cc44ff5728a5b1
Quentin Creuzet <quentin@f451.studio>
2026-01-07 16:59:37 +0100

    Adjust image quality settings in config.php
    Updated image quality settings for webp and jpg formats in the configuration.

--- www/site/config/config.php
+++ www/site/config/config.php
@@ -7,6 +7,7 @@ return [
   'tobimori.seo.canonicalBase' => '',
   'tobimori.seo.robots.indicator' => false,
   'tobimori.seo.robots.pageSettings' => false,
+  'tobimori.seo.robots.types' => [],
   'tobimori.seo.sitemap' => [
     'excludeTemplates' => []
   ],
@@ -17,40 +18,34 @@ return [
     ]
   ],
 
+  'panel' => [
+    'theme' => 'light'
+  ],
+
   'thumbs' => [
     'presets' => [],
     'srcsets' => [
       'webp' => [
-        '50w' => ['width' => 50, 'quality' => 90, 'format' => 'webp'],
-        '100w' => ['width' => 100, 'quality' => 90, 'format' => 'webp'],
-        '200w' => ['width' => 200, 'quality' => 90, 'format' => 'webp'],
-        '400w' => ['width' => 400, 'quality' => 90, 'format' => 'webp'],
-        '600w' => ['width' => 600, 'quality' => 90, 'format' => 'webp'],
-        '800w' => ['width' => 800, 'quality' => 90, 'format' => 'webp'],
-        '1000w' => ['width' => 1000, 'quality' => 90, 'format' => 'webp'],
-        '1200w' => ['width' => 1200, 'quality' => 90, 'format' => 'webp'],
-        '1400w' => ['width' => 1400, 'quality' => 90, 'format' => 'webp'],
-        '1600w' => ['width' => 1600, 'quality' => 90, 'format' => 'webp'],
-        '1800w' => ['width' => 1800, 'quality' => 90, 'format' => 'webp'],
-        '2000w' => ['width' => 2000, 'quality' => 90, 'format' => 'webp'],
-        '2200w' => ['width' => 2200, 'quality' => 90, 'format' => 'webp'],
-        '2560w' => ['width' => 2560, 'quality' => 90, 'format' => 'webp']
+        '100w' => ['width' => 100, 'quality' => 85, 'format' => 'webp'],
+        '200w' => ['width' => 200, 'quality' => 85, 'format' => 'webp'],
+        '400w' => ['width' => 400, 'quality' => 85, 'format' => 'webp'],
+        '800w' => ['width' => 800, 'quality' => 85, 'format' => 'webp'],
+        '1000w' => ['width' => 1000, 'quality' => 85, 'format' => 'webp'],
+        '1200w' => ['width' => 1200, 'quality' => 85, 'format' => 'webp'],
+        '1600w' => ['width' => 1600, 'quality' => 85, 'format' => 'webp'],
+        '2200w' => ['width' => 2200, 'quality' => 85, 'format' => 'webp'],
+        '2560w' => ['width' => 2560, 'quality' => 85, 'format' => 'webp']
       ],
       'jpg' => [
-        '50w' => ['width' => 50, 'quality' => 90, 'format' => 'jpg'],
-        '100w' => ['width' => 100, 'quality' => 90, 'format' => 'jpg'],
-        '200w' => ['width' => 200, 'quality' => 90, 'format' => 'jpg'],
-        '400w' => ['width' => 400, 'quality' => 90, 'format' => 'jpg'],
-        '600w' => ['width' => 600, 'quality' => 90, 'format' => 'jpg'],
-        '800w' => ['width' => 800, 'quality' => 90, 'format' => 'jpg'],
-        '1000w' => ['width' => 1000, 'quality' => 90, 'format' => 'jpg'],
-        '1200w' => ['width' => 1200, 'quality' => 90, 'format' => 'jpg'],
-        '1400w' => ['width' => 1400, 'quality' => 90, 'format' => 'jpg'],
-        '1600w' => ['width' => 1600, 'quality' => 90, 'format' => 'jpg'],
-        '1800w' => ['width' => 1800, 'quality' => 90, 'format' => 'jpg'],
-        '2000w' => ['width' => 2000, 'quality' => 90, 'format' => 'jpg'],
-        '2200w' => ['width' => 2200, 'quality' => 90, 'format' => 'jpg'],
-        '2560w' => ['width' => 2560, 'quality' => 90, 'format' => 'jpg']
+        '100w' => ['width' => 100, 'quality' => 85, 'format' => 'jpg'],
+        '200w' => ['width' => 200, 'quality' => 85, 'format' => 'jpg'],
+        '400w' => ['width' => 400, 'quality' => 85, 'format' => 'jpg'],
+        '800w' => ['width' => 800, 'quality' => 85, 'format' => 'jpg'],
+        '1000w' => ['width' => 1000, 'quality' => 85, 'format' => 'jpg'],
+        '1200w' => ['width' => 1200, 'quality' => 85, 'format' => 'jpg'],
+        '1600w' => ['width' => 1600, 'quality' => 85, 'format' => 'jpg'],
+        '2200w' => ['width' => 2200, 'quality' => 85, 'format' => 'jpg'],
+        '2560w' => ['width' => 2560, 'quality' => 85, 'format' => 'jpg']
       ],
     ]
   ],
